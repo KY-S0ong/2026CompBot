dc47bf43e5590d11af443f1dee0009b4bcbf0e60
KY-S0ong <ky.wong26@students.bchigh.edu>
2026-02-07 20:04:01 -0500

    Dr. Lax the monster slayer

--- src/main/java/frc/robot/subsystems/structures/Flywheel.java
+++ src/main/java/frc/robot/subsystems/structures/Flywheel.java
@@ -5,8 +5,12 @@
 package frc.robot.subsystems.structures;
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -17,7 +21,8 @@ public class Flywheel extends SubsystemBase {
 
   private double gearRatio = 1.0;
   private double height = 100;
-  private double fuelMass = 0.448;
+  private double fuelMassLBS = 0.448;
+  private double fuelMassKG = .2177243;
 
   public Flywheel() {
     // intakeShooterConfiguration.Inverted =
@@ -41,32 +46,58 @@ public class Flywheel extends SubsystemBase {
     intakeShooter.set(0);
   }
 
-  private double getShotVelocity(double distance) {
-    double shooterHoodRad = 0.99483;
-    double velocity =
-        Math.sqrt(
-            (-4.9 * Math.pow(distance, 2))
-                / (Math.pow(Math.cos(shooterHoodRad), 2) * (-height - Math.tan(shooterHoodRad))));
+  private double getTargetVelocity() {
+    double distance = SmartDashboard.getNumber("Shot Distance", 2.0);
+    double g = 9.806;
+    double theta = 0.99483; // Launch angle in radians
 
+    double velocity = (distance / Math.cos(theta)) * Math.sqrt((g / 2) * 1 / (Math.tan(theta) * distance - height));
     return velocity;
   }
 
-  public double getShotVolts() {
+  public double getTargetVolts() {
 
-    double distance = SmartDashboard.getNumber("Shot Distance", fuelMass);
+    double targetVel = getTargetVelocity();
 
-    double velocity = getShotVelocity(distance);
+    if (targetVel <= 0)
+      return 10.0;
 
-    double time = (velocity + Math.sqrt(Math.pow(velocity, 2) * (-2 * 9.8 * height))) / 9.8;
+    // Constants - you should tune these!
+    double kS = 0.4; // Volts to overcome friction
+    double kV = 0.5; // Volts per meter/second (example value)
 
-    double power = fuelMass * Math.pow(velocity, 2) / (2 * time);
+    double voltage = kS + (kV * targetVel);
 
-    return power / 40;
+    // Cap the voltage to the battery limit (12V)
+    return Math.min(voltage, 12.0);
+  }
+
+  public void TESTsetFlyWheelVelocity() {
+    intakeShooter.setControl(new VelocityVoltage(
+        getTargetVelocity() /
+            ((Math.PI * 2) * 4)));
   }
 
   private void SmartDashboardUpdate() {
     SmartDashboard.putNumber(
         "FlyWheel RPM", gearRatio * intakeShooter.getVelocity().getValueAsDouble());
-    SmartDashboard.putNumber("Desired Shot Voltage", getShotVolts());
+    SmartDashboard.putNumber("Desired Shot Voltage", getTargetVolts());
   }
+
+  /*
+   * private double AminTargetVelocity(){
+   * double distance = SmartDashboard.getNumber("Shot Distance", 2.0 );
+   * 
+   * double g = 9.806;
+   * double theta = 0.99483; // Launch angle in radians
+   * double deltaHeight = 2.64;
+   * 
+   * double velocity = Math.sqrt(
+   * (g * Math.pow(distance, 2)) /
+   * (2 * Math.pow(Math.cos(theta), 2) * (distance * Math.tan(theta) -
+   * deltaHeight)));
+   * 
+   * return velocity;
+   * }
+   */
 }
